921df06610d276989aedde07bcc085ce1a7cde68
tbach <tbach@cern.ch>
2016-10-08 02:03:51 +0200

    initial content of sqm project

---
+++ src/main/java/edu/hskl/projects/triangle/triangle/Triangle.java
@@ -0,0 +1,5 @@
+package edu.hskl.projects.triangle.triangle;
+
+public abstract class Triangle {
+	public abstract TriangleResult getTriangleResultForLenghts(int... lengths);
+}
---
+++ src/main/java/edu/hskl/projects/triangle/triangle/TriangleResult.java
@@ -0,0 +1,5 @@
+package edu.hskl.projects.triangle.triangle;
+
+public enum TriangleResult {
+	UNKNOWN, TRIANGLE_NOT_EQUAL, TRIANGLE_EQUILATERAL, TRIANGLE_ISOSCELES;
+}
