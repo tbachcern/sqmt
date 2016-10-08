2799b7c31e3d45d810d0f37d6308c2335a2bc4b2
cRtnsQ <cabl0002@stud.hs-kl.de>
2016-10-08 11:44:26 +0200

    Tests und Implementierung

---
+++ src/main/java/edu/hskl/projects/triangle/triangle/TriangleCBloching.java
@@ -0,0 +1,77 @@
+package edu.hskl.projects.triangle.triangle;
+
+
+public class TriangleCBloching extends Triangle {
+
+	@Override
+	public TriangleResult getTriangleResultForLenghts(int... lengths) {
+		
+		if(checkLength(lengths) == true){
+			
+			int a = lengths[0];
+			int b = lengths[1];
+			int c = lengths[2];
+			
+			if(checkIsTriangle(a, b, c) == true){
+				if(checkNotEqualOrLessZero(a, b, c) == true){
+					if(checkEQUILATERAL(a, b, c) == true){
+						return TriangleResult.TRIANGLE_EQUILATERAL;
+					}else{
+						if(checkISOSCELES(a, b, c) == true) {
+							return TriangleResult.TRIANGLE_ISOSCELES;
+						}else{
+							return TriangleResult.TRIANGLE_NOT_EQUAL;
+						}
+					}
+				}else{
+					return TriangleResult.UNKNOWN;
+				}
+			}else{
+				return TriangleResult.UNKNOWN;
+			}
+		}
+		return TriangleResult.UNKNOWN;
+	}
+
+	private boolean checkLength(int... l) {
+		if (l.length == 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private boolean checkIsTriangle(int ta, int tb, int tc){
+		if(ta + tb > tc && ta+tc > tb && tb+tc > ta){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private boolean checkNotEqualOrLessZero(int ta, int tb, int tc){
+		if(ta <= 0 || tb <= 0 || tc <= 0){
+			return false;
+		}
+		return true;
+	}
+
+
+	private boolean checkEQUILATERAL(int ta, int tb, int tc) {
+		if(ta == tb && tb == tc){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private boolean checkISOSCELES(int ta, int tb, int tc) {
+		if(ta == tb || ta == tc || tb == tc){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+
+}
---
+++ src/test/java/edu/hskl/projects/triangle/triangle/TriangleCBlochingTest.java
@@ -0,0 +1,48 @@
+package edu.hskl.projects.triangle.triangle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TriangleCBlochingTest {
+	
+	private Triangle triangle;
+	
+	@Before
+	public void setUp() throws Exception {
+		triangle = new TriangleCBloching();
+	}
+
+	@Test
+	public void testGetTriangleResultForLenghtsEquilateral() throws Exception {
+		int [] input = {10,10,10};
+		TriangleResult expected = TriangleResult.TRIANGLE_EQUILATERAL;
+		assertThat(triangle.getTriangleResultForLenghts(input)).isEqualTo(expected);	
+	}
+	@Test
+	public void testGetTriangleResultForLenghtsEquilateral2() throws Exception {
+		int [] input = {11,11,11};
+		TriangleResult expected = TriangleResult.TRIANGLE_EQUILATERAL;
+		assertThat(triangle.getTriangleResultForLenghts(input)).isEqualTo(expected);	
+	}
+	@Test
+	public void testGetTriangleResultForLenghtsNotEqual() throws Exception {
+		int [] input = {13,12,5};
+		TriangleResult expected = TriangleResult.TRIANGLE_NOT_EQUAL;
+		assertThat(triangle.getTriangleResultForLenghts(input)).isEqualTo(expected);	
+	}
+	@Test
+	public void testGetTriangleResultForLenghtsISOSCELES() throws Exception {
+		int [] input = {15,15,11};
+		TriangleResult expected = TriangleResult.TRIANGLE_ISOSCELES;
+		assertThat(triangle.getTriangleResultForLenghts(input)).isEqualTo(expected);	
+	}
+	@Test
+	public void testGetTriangleResultForLenghtsUnknown() throws Exception {
+		int [] input = {13,12,60};
+		TriangleResult expected = TriangleResult.UNKNOWN;
+		assertThat(triangle.getTriangleResultForLenghts(input)).isEqualTo(expected);	
+	}
+
+}
